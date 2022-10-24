9e528281691c3245965c7709bbf2bb5ca6c869b4
Betelihem Waldron <110792525+wald0084@users.noreply.github.com>
2022-10-24 16:41:54 +1030

    Final_code_complete

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 A docassemble extension.
 
-## Author
+# Application description
+JusticeGuide SA is a service directory for JusticeNet (a not-for-profit enterprise). It aims to connect clients with legal issues to the relevant organisations applicable to the circumstances of their personal issue, and related financial and social circumstances. JusticeGuide SA acts as a bridge between those in need and those who can help. It is necessary to address the gap between the average person of limited technological knowledge, and the legal profession. The application categorises information from clients to help them identify appropriate services.
 
-Lauren Docking, dock0013@flinders.edu.au 
-Beti Waldron, wald0084@flinders.edu.au
+The application exists as a filter that ensures those reaching out to JusticeNet SA do so as a last resort due to ineligibility for other legal services. JusticeNet SA typically works with those of varied ages from 19 years or older and JusticeGuide SA ensures users of non-technological backgrounds are able to feel connected and heard.
+
+JusticeGuide SA is a program that primarily functions as an interactive service directory that displays relevant organisations to the users’ issues based on data input.
+
+
+
 
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.LLAW33012022S2JNSA1',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.LLAW33012022S2JNSA1\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nLauren Docking, dock0013@flinders.edu.au \r\nBeti Waldron, wald0084@flinders.edu.au\r\n\r\n',
+      long_description='# docassemble.LLAW33012022S2JNSA1\r\n\r\nA docassemble extension.\r\n\r\n# Application description\r\nJusticeGuide SA is a service directory for JusticeNet (a not-for-profit enterprise). It aims to connect clients with legal issues to the relevant organisations applicable to the circumstances of their personal issue, and related financial and social circumstances. JusticeGuide SA acts as a bridge between those in need and those who can help. It is necessary to address the gap between the average person of limited technological knowledge, and the legal profession. The application categorises information from clients to help them identify appropriate services.\r\n\r\nThe application exists as a filter that ensures those reaching out to JusticeNet SA do so as a last resort due to ineligibility for other legal services. JusticeNet SA typically works with those of varied ages from 19 years or older and JusticeGuide SA ensures users of non-technological backgrounds are able to feel connected and heard.\r\n\r\nJusticeGuide SA is a program that primarily functions as an interactive service directory that displays relevant organisations to the users’ issues based on data input.\r\n\r\n\r\n\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Lauren Docking',
       author_email='dock0013@flinders.edu.au',
